eb34c42b4419bbc7eaf50febcdcd2ba5e48412e0
Buddhi Nagahawatte <SGARNABN@rhc.msig.com>
2019-10-04 11:56:01 +0800

    working

--- src/main/java/com/buddhi/oauth2/AuthorizationServerConfig.java
+++ src/main/java/com/buddhi/oauth2/AuthorizationServerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
-@EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
